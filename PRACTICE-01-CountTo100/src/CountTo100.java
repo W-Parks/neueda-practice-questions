de84233482b1b21abc55eae72440b96b7a061def
Will Parks <45174202+W-Parks@users.noreply.github.com>
2018-12-07 10:16:14 +0000

    Completed Project

--- PRACTICE-01-CountTo100/src/CountTo100.java
+++ PRACTICE-01-CountTo100/src/CountTo100.java
@@ -4,6 +4,10 @@ public class CountTo100 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		for (int i = 0; i <= 100; i++) {
+			System.out.println(i);
+		}
+		
 	}
 
 }
