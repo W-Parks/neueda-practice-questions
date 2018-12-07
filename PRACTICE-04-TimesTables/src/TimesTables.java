341b59c2eb554d1938687afe7a484cbce746a293
Will Parks <45174202+W-Parks@users.noreply.github.com>
2018-12-07 12:29:28 +0000

    Question 4 answered

---
+++ PRACTICE-04-TimesTables/src/TimesTables.java
@@ -0,0 +1,18 @@
+
+public class TimesTables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1, num2;
+		
+		for (num1 = 1; num1 <= 12; num1++) {
+			for (num2 = 1; num2 <= 12; num2++) {
+				System.out.println(num1 + " x " + num2);
+			}
+		}
+		
+		
+	}
+
+}
